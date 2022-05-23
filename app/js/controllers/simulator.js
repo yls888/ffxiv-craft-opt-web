5b3ac47ce135002ef75b3a4ec9c6951322fd7d06
X-bear <522168288@qq.com>
2022-05-23 19:44:15 +0800

    fix 俭约不work

--- app/js/controllers/simulator.js
+++ app/js/controllers/simulator.js
@@ -230,7 +230,7 @@
             var cpExceeded = wastedAction && _actionsByName[action].cpCost > $scope.simulatorStatus.state.cp;
 
             return {
-                'faded-icon': !$scope.isActionSelected(action, cls) || $rootScope.disableClass(action,index, $scope.sequence),
+                'faded-icon': !$scope.isActionSelected(action, cls) || $rootScope.prudentActionInWasteNot(action,index, $scope.sequence),
                 'wasted-action': wastedAction,
                 'action-no-cp': cpExceeded
             };
