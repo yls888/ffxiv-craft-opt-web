b291f079ad3d3edc35f80a4647a23cda3d3d23e6
yls888 <conan08@vip.qq.com>
2023-01-15 01:09:20 +0800

    Update ffxivcraftmodel.js
    修复一个因取整导致计算精度不准确的问题（JS全锅）

--- app/js/ffxivcraftmodel.js
+++ app/js/ffxivcraftmodel.js
@@ -86,7 +86,7 @@ function Synth(crafter, recipe, maxTrickUses, reliabilityIndex, useConditions, m
 Synth.prototype.calculateBaseProgressIncrease = function (effCrafterLevel, craftsmanship) {
     var baseValue = (craftsmanship * 10) / this.recipe.progressDivider + 2;
     if (effCrafterLevel <= this.recipe.level) {
-        baseValue *= (this.recipe.progressModifier || 100) * Math.fround(0.01);
+        baseValue *= (this.recipe.progressModifier || 100) * 0.01;
     }
     return Math.floor(baseValue);
 };
@@ -94,7 +94,7 @@ Synth.prototype.calculateBaseProgressIncrease = function (effCrafterLevel, craft
 Synth.prototype.calculateBaseQualityIncrease = function (effCrafterLevel, control) {
     var baseValue = (control * 10) / this.recipe.qualityDivider + 35;
     if (effCrafterLevel <= this.recipe.level) {
-        baseValue *= (this.recipe.qualityModifier || 100) * Math.fround(0.01);
+        baseValue *= (this.recipe.qualityModifier || 100) * 0.01;
     }
     return Math.floor(baseValue);
 };
