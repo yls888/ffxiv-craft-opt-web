d19a04404f383857fffa682b796296ed9edc2d93
yls888 <conan08@vip.qq.com>
2023-05-26 20:35:18 +0800

    Update ffxivcraftmodel.js

--- app/js/ffxivcraftmodel.js
+++ app/js/ffxivcraftmodel.js
@@ -292,25 +292,15 @@ function ApplyModifiers(s, action, condition) {
             cpCost = 18;
         }
     }
-
     // Add combo bonus following Basic Touch
     if (isActionEq(action, AllActions.standardTouch)) {
         if (s.action === AllActions.basicTouch.shortName) {
             cpCost = 18;
-        }
-        if (s.action != AllActions.basicTouch.shortName) {
-            s.wastedActions += 5;
-        }
-    }
-
-    // Add combo bonus following Standard Touch
-    if (isActionEq(action, AllActions.advancedTouch)) {
-        if (s.action === AllActions.standardTouch.shortName) {
-            cpCost = 18;
+            s.wastedActions -= 0.05;
             s.touchComboStep = 1;
         }
-        if (s.action != AllActions.standardTouch.shortName) {
-            s.wastedActions += 10;
+        if (s.action === AllActions.standardTouch.shortName) {
+            s.wastedActions += 0.1;
         }
     }
 
